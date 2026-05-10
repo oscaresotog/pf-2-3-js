03c1f6bf51aca14443665d2efa4ffcb4aabd48ab
Oscar Soto <oscar.e.soto.g.ing@gmail.com>
2026-05-09 23:12:50 -0600

    Se realizo correctamente el ejercicio fizzBuzz

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,23 @@
 export function fizzbuzz(){
   //tu código va aqui
- 
-  
+
+let contador = 1;
+let multiplo3 = 3;
+
+while(contador <= 100){
+
+if (contador % 3 === 0 && contador % 5 === 0){
+  console.log("FizzBuzz")
+}else if(contador % 3 === 0){
+  console.log("Fizz")
+}else if(contador % 5 === 0){
+  console.log("Buzz")
+}else{
+  console.log(contador)
+}
+contador++;
+}
+
 }
 
 fizzbuzz();
